c9944dbca53c60113b94f6abe2e87eab637d910e
ashwith31 <ashwitash@gmail.com>
2021-08-17 20:31:46 +0530

    modified:Used compareto method to find maximum of three string

--- src/com/genericsprograms/TestMaximum.java
+++ src/com/genericsprograms/TestMaximum.java
@@ -53,11 +53,32 @@ public class TestMaximum {
 		}
 		System.out.println(max);
 	}
-
+	/*
+	 * This method finds the maximum of three Float objects. There is no return type
+	 * or parameters passed.
+	 */
+	public static void findMaxString() {
+		// Initializing objects.
+		String x, y, z;
+		System.out.println("Enter three Strings:");
+		x = sc.next();
+		y = sc.next();
+		z = sc.next();
+		// assuming x to be the maximum object.
+		String max = x;
+		if (max.compareTo(y) < 0) {
+			max = y;
+		}
+		if (max.compareTo(z) < 0) {
+			max = z;
+		}
+		System.out.println(max);
+	}
 	public static void main(String[] args) {
 
 		findMaxInt();
 		findMaxFloat();
+		findMaxString();
 
 	}
 }
