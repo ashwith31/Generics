e318112fe41275cb09cfc7823a1fb24818539721
ashwith31 <ashwitash@gmail.com>
2021-08-20 13:26:29 +0530

    modified:Created a Generic Class

--- src/com/genericsprograms/TestMaximum.java
+++ src/com/genericsprograms/TestMaximum.java
@@ -6,7 +6,18 @@ import java.util.Scanner;
  * Program to test the maximum of values.
  * @author Ashwith
  */
-public class TestMaximum {
+public class TestMaximum<Z extends Comparable<Z>> {
+
+	Z first, second, third;
+
+	public TestMaximum(Z first, Z second, Z third) {
+		this.first = first;
+		this.second = second;
+		this.third = third;
+		// calling the static method inside constructor
+		testMaximum(first, second, third);
+	}
+
 	/*
 	 * This method uses the concept of Generics and finds the maximum of three of
 	 * them.
@@ -15,26 +26,21 @@ public class TestMaximum {
 	 * 
 	 * @return returns the maximum object.
 	 */
-	public static <Z extends Comparable<Z>> Z findMaximum(Z first, Z second, Z third) {
+	public static <Z extends Comparable<Z>> void testMaximum(Z first, Z second, Z third) {
 		// assuming first to be maximum
 		Z maximum = first;
 		if (second.compareTo(maximum) > 0)
 			maximum = second;
 		if (third.compareTo(maximum) > 0)
 			maximum = third;
-		return maximum;
+		System.out.println(maximum);
+
 	}
 
-	/*
-	 * Calling the findMaximum method in the main method
-	 */
 	public static void main(String[] args) {
 
-		Integer num1 = 86, num2 = 99, num3 = 101;
-		System.out.println("Maximum value of three Integer Variables :" + findMaximum(num1, num2, num3));
-		Float f1 = 56.35f, f2 = 102.55f, f3 = 45.96f;
-		System.out.println("Maximum value of three Float Variables :" + findMaximum(f1, f2, f3));
-		String name1 = "Apple", name2 = "Peach", name3 = "Banana";
-		System.out.println("Maximum value of three String Variables :" + findMaximum(name1, name2, name3));
+		new TestMaximum(11, 28, 35);
+		new TestMaximum("Apple", " Peach", "Banana");
+		new TestMaximum(1.3, 11.3, 111.3);
 	}
 }
